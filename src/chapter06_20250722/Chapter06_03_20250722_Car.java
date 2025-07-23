b02e54b82af7ad31da5fd930837c4c0c0703e5ab
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-07-23 14:52:30 +0900

    매소드 리턴 타입

--- src/chapter06_20250722/Chapter06_03_20250722_Car.java
+++ src/chapter06_20250722/Chapter06_03_20250722_Car.java
@@ -2,12 +2,17 @@ package chapter06_20250722;
 
 public class Chapter06_03_20250722_Car {
 	
+	//필드
 	Chapter06_03_20250722_Engine engine;
 	int maxspeed;
 	String name;
 	
 	
+	// 기본 생성자
+	public Chapter06_03_20250722_Car() {
 
+
+	}
 
 	public Chapter06_03_20250722_Car(Chapter06_03_20250722_Engine engine, int maxspeed, String name) {
 		super();
@@ -17,14 +22,24 @@ public class Chapter06_03_20250722_Car {
 	}
 
 
-	public Chapter06_03_20250722_Car() {
-
-
-	}
-
-
+	//매소드
 	public void carEngine() {
 		System.out.println("엔진 타입 : " + engine.type);
 		engine.printEngine();
+	}
+	
+	// cars를 return하는데 cars의 타입이 무엇인가? String[]. 그래서 public String[] 인거다.
+	public String[] outputCats(){
+		String[] cars = {"소나타", "아반테", "k5"};
+		return cars;
+	}
+	
+	// engine를 return하는데 engine의 타입이 무엇인가? Chapter06_03_20250722_Engine. 
+	// 그래서 Chapter06_03_20250722_Engine 인거다.
+	// return하는 타입이 뭔지 잘 봐라.
+	public Chapter06_03_20250722_Engine outputEngine() {
+		engine.type = "디젤";
+		engine.displacement = 2;	
+		return engine;
 	}
 }
