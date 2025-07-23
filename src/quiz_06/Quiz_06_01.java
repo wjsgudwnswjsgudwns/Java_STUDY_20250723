bad539dc7e4bf77fecb3c41e35a3816528fa7433
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-07-23 15:16:31 +0900

    06_4 퀴즈

---
+++ src/quiz_06/Quiz_06_01.java
@@ -0,0 +1,13 @@
+package quiz_06;
+
+public class Quiz_06_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1 . 리턴값이 없는 메소드는 리턴 타입을 (void)로 해야한다.");
+		System.out.println("2 . 리턴 타입이 있는 메소드는 리턴값을 지정하기 위해 반드시 (return)문이 이어야 한다.");
+		System.out.println("3 . 매개값의 개수를 모르는 경우 (배열)을 이용해서 매개 변수를 선언할 수 있다.");
+		System.out.println("4 . 같은 이름의 메소드를 여러개 선언하는 것을 메소드 (오버로딩)이라고 한다.");
+	}
+
+}
---
+++ src/quiz_06/Quiz_06_02.java
@@ -0,0 +1,15 @@
+package quiz_06;
+
+public class Quiz_06_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("메소드 오버로딩에 대한 설명으로 맞는 것에 O표, 틀린 것에 X표 하세요.");
+		System.out.println("1 . 동일한 이름의 메소드를 여러 개 선언하는 것을 말한다. ( O )");
+		System.out.println("2 . 반드시 리턴 타입이 달라야 한다. ( X )");
+		System.out.println("3 . 매개 변수의 타입, 개수, 순서를 다르게 선언해야 한다. ( O )");
+		System.out.println("4 . 매개값의 타입 및 개수에 따라 호출될 메소드가 선택된다. ( O )");
+	}
+
+}
