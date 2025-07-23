16e9e47ec55b95edf378c03cb67d11a8d9bc92ad
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-07-23 17:52:45 +0900

    수정

--- src/ex_20250723/Ex_05_20250723.java
+++ src/ex_20250723/Ex_05_20250723.java
@@ -7,8 +7,8 @@ public class Ex_05_20250723 {
 		Rectangle rectangle1 = new Rectangle(1, 2);
 		Rectangle rectangle2 = new Rectangle(1.5, 2.3);
 		
-		System.out.println(rectangle1.round());
-		System.out.println(rectangle2.round());
+		System.out.println("사각형 둘레 길이 : " + rectangle1.round());
+		System.out.println("사각형 둘레 길이 : " + rectangle2.round());
 	}
 
 }
